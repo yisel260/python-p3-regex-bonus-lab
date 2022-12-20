8bf00056e3c85982e56b6f613affd5e2abfa0778
Ben Botsford <ben.botsford@flatironschool.com>
2022-12-20 11:09:09 -0600

    remove bad solution

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,5 @@
 import re
 
-my_pattern = r"It's such a lovely day today.|Some weather we're having today, huh\?|Maybe today's just not my day."
+my_pattern = r""
 my_regex = re.compile(my_pattern)
 
