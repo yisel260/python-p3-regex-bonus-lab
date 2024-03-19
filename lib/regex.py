6c168cf721a6850b60fda96407d6cb7262f6b02b
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-03-18 20:28:07 -0400

    completed lab

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,7 @@
 import re
 
-my_pattern = r""
+#my_pattern = r"[\w+\s\'\.\,\?]+"
+my_pattern = r"It's such a lovely day today\.|Some weather we're having today, huh\?|Maybe today's just not my day\."
+
 my_regex = re.compile(my_pattern)
 
